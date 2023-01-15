fc8b7223380b3dbb62491f4975f622e5b261d294
Pkiyach <118667256+Pkiyach@users.noreply.github.com>
2023-01-15 18:32:51 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,603 @@
+
+
+
+
+// INITIALISING A MAP
+var mapView = new ol.View({
+    center: ol.proj.fromLonLat([36.95855799006071,-0.39154791898703833  ]),
+    zoom: 17
+});
+
+
+
+
+var map = new ol.Map({
+    target: 'map',
+    view: mapView,
+    controls: []
+});
+
+
+
+
+//DISPLAY THE TILELAYER
+var noneTile = new ol.layer.Tile({
+    title: 'None',
+    type: 'base',
+    visible: false
+});
+
+var osmTile = new ol.layer.Tile({
+    title: 'Open Street Map',
+    visible: true,
+    type: 'base',
+    source: new ol.source.OSM()
+});
+
+//map.addLayer(osmTile) ;
+
+var baseGroup = new ol.layer.Group({
+    title: 'Base Maps',
+    fold: true,
+    layers: [osmTile, noneTile]
+});
+
+map.addLayer(baseGroup);
+
+
+//ADD spatial  data FROM  GEOSERVER
+var Road = new ol.layer.Tile({
+    title: "Road",
+    source: new ol.source.TileWMS({
+        url: 'http://localhost:8080/geoserver/N_Kimathi/wms',
+        params: { 'LAYERS': 'N_Kimathi:MainRoad', 'TILED': true },
+        serverType: 'geoserver',
+        visible: true
+    })
+});
+
+var Buildings = new ol.layer.Tile({
+    title: "Building",
+    source: new ol.source.TileWMS({
+        url: 'http://localhost:8080/geoserver/N_Kimathi/wms',
+        params: { 'LAYERS': 'N_Kimathi:Merged_Polygon', 'TILED': true },
+        serverType: 'geoserver',
+        visible: true
+    })
+});
+
+var Points = new ol.layer.Tile({
+    title: "Points",
+    source: new ol.source.TileWMS({
+        url: 'http://localhost:8080/geoserver/N_Kimathi/wms',
+        params: { 'LAYERS': 'N_Kimathi:Merged_points', 'TILED': true },
+        serverType: 'geoserver',
+        visible: true
+    })
+});
+
+// map.addLayer(Road);
+
+var overlayGroup = new ol.layer.Group({
+    title: 'Overlays',
+    fold: true,
+    layers: [Road,Buildings,Points]
+});
+
+ map.addLayer(overlayGroup);
+
+
+
+ //geoserver layer switcher 
+
+var layerSwitcher = new ol.control.LayerSwitcher({
+    activationMode: 'click',
+    startActive: false,
+    groupSelectStyle: 'children'
+});
+
+map.addControl(layerSwitcher);
+
+
+
+
+
+//corodinates  of the area mouse is positioned
+
+var mousePosition = new ol.control.MousePosition({
+    className: 'mousePosition',
+    projection: 'EPSG:4326',
+    coordinateFormat: function (coordinate) { return ol.coordinate.format(coordinate, '{y} , {x}', 6); }
+});
+map.addControl(mousePosition);
+
+
+
+
+
+
+// start : zoomIn Control
+
+var zoomInInteraction = new ol.interaction.DragBox();
+
+zoomInInteraction.on('boxend', function () {
+    var zoomInExtent = zoomInInteraction.getGeometry().getExtent();
+    map.getView().fit(zoomInExtent);
+});
+
+var ziButton = document.createElement('button');
+ziButton.innerHTML = '<img src="resources/images/zoomIn.svg" alt="" style="width:18px;height:18px;transform: rotate(270deg);filter:brightness(0) invert(1);vertical-align:middle"></img>';
+ziButton.className = 'myButton';
+ziButton.id = 'ziButton';
+
+var ziElement = document.createElement('div');
+ziElement.className = 'ziButtonDiv';
+ziElement.appendChild(ziButton);
+
+var ziControl = new ol.control.Control({
+    element: ziElement
+})
+
+var zoomInFlag = false;
+ziButton.addEventListener("click", () => {
+    ziButton.classList.toggle('clicked');
+    zoomInFlag = !zoomInFlag;
+    if (zoomInFlag) {
+        document.getElementById("map").style.cursor = "zoom-in";
+        map.addInteraction(zoomInInteraction);
+    } else {
+        map.removeInteraction(zoomInInteraction);
+        document.getElementById("map").style.cursor = "default";
+    }
+})
+
+map.addControl(ziControl);
+
+// end : zoomIn Control
+
+// start : zoomOut Control
+
+var zoomOutInteraction = new ol.interaction.DragBox();
+
+zoomOutInteraction.on('boxend', function () {
+    var zoomOutExtent = zoomOutInteraction.getGeometry().getExtent();
+    map.getView().setCenter(ol.extent.getCenter(zoomOutExtent));
+
+    mapView.setZoom(mapView.getZoom() - 1)
+});
+
+var zoButton = document.createElement('button');
+zoButton.innerHTML = '<img src="resources/images/zoomOut.png" alt="" style="width:18px;height:18px;transform: rotate(270deg);filter:brightness(0) invert(1);vertical-align:middle"></img>';
+zoButton.className = 'myButton';
+zoButton.id = 'zoButton';
+
+var zoElement = document.createElement('div');
+zoElement.className = 'zoButtonDiv';
+zoElement.appendChild(zoButton);
+
+var zoControl = new ol.control.Control({
+    element: zoElement
+})
+
+var zoomOutFlag = false;
+zoButton.addEventListener("click", () => {
+    zoButton.classList.toggle('clicked');
+    zoomOutFlag = !zoomOutFlag;
+    if (zoomOutFlag) {
+        document.getElementById("map").style.cursor = "zoom-out";
+        map.addInteraction(zoomOutInteraction);
+    } else {
+        map.removeInteraction(zoomOutInteraction);
+        document.getElementById("map").style.cursor = "default";
+    }
+})
+
+map.addControl(zoControl);
+
+// end : zoomOut Control
+
+
+// start : Geolocation control
+
+// end : Geolocation Control
+
+
+
+// start : attribute query
+
+var geojson;
+var featureOverlay;
+
+var qryButton = document.createElement('button');
+qryButton.innerHTML ='<img src="resources/images/query.svg" alt="" style="width:17px;height:17px;filter:brightness(0) invert(1);vertical-align:middle"></img>';
+qryButton.className ='myButton';
+qryButton.id = 'qryButton';
+
+var qryElement = document.createElement('div');
+qryElement.className = 'myButtonDiv';
+qryElement.appendChild(qryButton);
+
+var qryControl = new ol.control.Control({
+    element: qryElement
+})
+
+var qryFlag = false;
+qryButton.addEventListener("click", () => {
+    // disableOtherInteraction('lengthButton');
+    qryButton.classList.toggle('clicked');
+    qryFlag = !qryFlag;
+    document.getElementById("map").style.cursor = "default";
+    if (qryFlag) {
+        if (geojson) {
+            geojson.getSource().clear();
+            map.removeLayer(geojson);
+        }
+
+        if (featureOverlay) {
+            featureOverlay.getSource().clear();
+            map.removeLayer(featureOverlay);
+        }
+        document.getElementById("attQueryDiv").style.display = "block";
+
+        bolIdentify = false;
+
+        addMapLayerList();
+    } else {
+        document.getElementById("attQueryDiv").style.display = "none";
+        document.getElementById("attListDiv").style.display = "none";
+
+        if (geojson) {
+            geojson.getSource().clear();
+            map.removeLayer(geojson);
+        }
+
+        if (featureOverlay) {
+            featureOverlay.getSource().clear();
+            map.removeLayer(featureOverlay);
+        }
+    }
+
+})
+
+map.addControl(qryControl);
+
+function addMapLayerList() {
+    $(document).ready(function () {
+        $.ajax({
+            type: "GET",
+            url: "http://localhost:8080/geoserver/N_Kimathi/wfs?request=GetCapabilities",
+            dataType: "xml",
+            success: function (xml) {
+                var select = $('#selectLayer');
+                select.append("<option class='ddindent' value=''></option>");
+                $(xml).find('FeatureType').each(function () {
+                    $(this).find('Name').each(function () {
+                        var value = $(this).text();
+                        select.append("<option class='ddindent' value='" + value + "'>" + value + "</option>");
+                    });
+                });
+            }
+        });
+    });
+
+};
+
+
+$(function () {
+    document.getElementById("selectLayer").onchange = function () {
+        var select = document.getElementById("selectAttribute");
+        while (select.options.length > 0) {
+            select.remove(0);
+        }
+        var value_layer = $(this).val();
+        $(document).ready(function () {
+            $.ajax({
+                type: "GET",  
+                url: " http://localhost:8080/geoserver/wfs?request=DescribeFeatureType&version=1.1.0&typename=N_Kimathi:	MainR" + value_layer,
+                dataType: "xml",
+                success: function (xml) {
+
+                    var select = $('#selectAttribute');
+                    //var title = $(xml).find('xsd\\:complexType').attr('name');
+                    //	alert(title);
+                    select.append("<option class='ddindent' value=''></option>");
+                    $(xml).find('xsd\\:sequence').each(function () {
+
+                        $(this).find('xsd\\:element').each(function () {
+                            var value = $(this).attr('name');
+                            //alert(value);
+                            var type = $(this).attr('type');
+                            //alert(type);
+                            if (value != 'geom' && value != 'the_geom') {
+                                select.append("<option class='ddindent' value='" + type + "'>" + value + "</option>");
+                            }
+                        });
+
+                    });
+                }
+            });
+        });
+    }
+    document.getElementById("selectAttribute").onchange = function () {
+        var operator = document.getElementById("selectOperator");
+        while (operator.options.length > 0) {
+            operator.remove(0);
+        }
+
+        var value_type = $(this).val();
+        // alert(value_type);
+        var value_attribute = $('#selectAttribute option:selected').text();
+        operator.options[0] = new Option('Select operator', "");
+
+        if (value_type == 'xsd:short' || value_type == 'xsd:int' || value_type == 'xsd:double') {
+            var operator1 = document.getElementById("selectOperator");
+            operator1.options[1] = new Option('Greater than', '>');
+            operator1.options[2] = new Option('Less than', '<');
+            operator1.options[3] = new Option('Equal to', '=');
+        }
+        else if (value_type == 'xsd:string') {
+            var operator1 = document.getElementById("selectOperator");
+            operator1.options[1] = new Option('Like', 'Like');
+            operator1.options[2] = new Option('Equal to', '=');
+        }
+    }
+
+    document.getElementById('attQryRun').onclick = function () {
+        map.set("isLoading", 'YES');
+
+        if (featureOverlay) {
+            featureOverlay.getSource().clear();
+            map.removeLayer(featureOverlay);
+        }
+
+        var layer = document.getElementById("selectLayer");
+        var attribute = document.getElementById("selectAttribute");
+        var operator = document.getElementById("selectOperator");
+        var txt = document.getElementById("enterValue");
+
+        if (layer.options.selectedIndex == 0) {
+            alert("Select Layer");
+        } else if (attribute.options.selectedIndex == -1) {
+            alert("Select Attribute");
+        } else if (operator.options.selectedIndex <= 0) {
+            alert("Select Operator");
+        } else if (txt.value.length <= 0) {
+            alert("Enter Value");
+        } else {
+            var value_layer = layer.options[layer.selectedIndex].value;
+            var value_attribute = attribute.options[attribute.selectedIndex].text;
+            var value_operator = operator.options[operator.selectedIndex].value;
+            var value_txt = txt.value;
+            if (value_operator == 'Like') {
+                value_txt = "%25" + value_txt + "%25";
+            }
+            else {
+                value_txt = value_txt;
+            }
+            var url = "http://localhost:8080/geoserver/GISSimplified/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=" + value_layer + "&CQL_FILTER=" + value_attribute + "+" + value_operator + "+'" + value_txt + "'&outputFormat=application/json"
+            // console.log(url);
+            newaddGeoJsonToMap(url);
+            newpopulateQueryTable(url);
+            setTimeout(function () { newaddRowHandlers(url); }, 300);
+            map.set("isLoading", 'NO');
+        }
+    }
+});
+
+function newaddGeoJsonToMap(url) {
+
+    if (geojson) {
+        geojson.getSource().clear();
+        map.removeLayer(geojson);
+    }
+
+    var style = new ol.style.Style({
+        // fill: new ol.style.Fill({
+        //   color: 'rgba(0, 255, 255, 0.7)'
+        // }),
+        stroke: new ol.style.Stroke({
+            color: '#FFFF00',
+            width: 3
+        }),
+        image: new ol.style.Circle({
+            radius: 7,
+            fill: new ol.style.Fill({
+                color: '#FFFF00'
+            })
+        })
+    });
+
+    geojson = new ol.layer.Vector({
+        source: new ol.source.Vector({
+            url: url,
+            format: new ol.format.GeoJSON()
+        }),
+        style: style,
+
+    });
+
+    geojson.getSource().on('addfeature', function () {
+        map.getView().fit(
+            geojson.getSource().getExtent(),
+            { duration: 1590, size: map.getSize(), maxZoom: 21 }
+        );
+    });
+    map.addLayer(geojson);
+};
+
+function newpopulateQueryTable(url) {
+    if (typeof attributePanel !== 'undefined') {
+        if (attributePanel.parentElement !== null) {
+            attributePanel.close();
+        }
+    }
+    $.getJSON(url, function (data) {
+        var col = [];
+        col.push('id');
+        for (var i = 0; i < data.features.length; i++) {
+
+            for (var key in data.features[i].properties) {
+
+                if (col.indexOf(key) === -1) {
+                    col.push(key);
+                }
+            }
+        }
+
+        var table = document.createElement("table");
+
+        table.setAttribute("class", "table table-bordered table-hover table-condensed");
+        table.setAttribute("id", "attQryTable");
+        // CREATE HTML TABLE HEADER ROW USING THE EXTRACTED HEADERS ABOVE.
+
+        var tr = table.insertRow(-1);                   // TABLE ROW.
+
+        for (var i = 0; i < col.length; i++) {
+            var th = document.createElement("th");      // TABLE HEADER.
+            th.innerHTML = col[i];
+            tr.appendChild(th);
+        }
+
+        // ADD JSON DATA TO THE TABLE AS ROWS.
+        for (var i = 0; i < data.features.length; i++) {
+            tr = table.insertRow(-1);
+            for (var j = 0; j < col.length; j++) {
+                var tabCell = tr.insertCell(-1);
+                if (j == 0) { tabCell.innerHTML = data.features[i]['id']; }
+                else {
+                    tabCell.innerHTML = data.features[i].properties[col[j]];
+                }
+            }
+        }
+
+        // var tabDiv = document.createElement("div");
+        var tabDiv = document.getElementById('attListDiv');
+
+        var delTab = document.getElementById('attQryTable');
+        if (delTab) {
+            tabDiv.removeChild(delTab);
+        }
+
+        tabDiv.appendChild(table);
+
+        document.getElementById("attListDiv").style.display = "block";
+    });
+
+};
+
+var highlightStyle = new ol.style.Style({
+    fill: new ol.style.Fill({
+      color: 'rgba(255,0,255,0.3)',
+    }),
+    stroke: new ol.style.Stroke({
+        color: '#FF00FF',
+        width: 3,
+    }),
+    image: new ol.style.Circle({
+        radius: 10,
+        fill: new ol.style.Fill({
+            color: '#FF00FF'
+        })
+    })
+});
+
+
+var featureOverlay = new ol.layer.Vector({
+    source: new ol.source.Vector(),
+    map: map,
+    style: highlightStyle
+});
+
+function newaddRowHandlers() {
+    var table = document.getElementById("attQryTable");
+    var rows = document.getElementById("attQryTable").rows;
+    var heads = table.getElementsByTagName('th');
+    var col_no;
+    for (var i = 0; i < heads.length; i++) {
+        // Take each cell
+        var head = heads[i];
+        if (head.innerHTML == 'id') {
+            col_no = i + 1;
+        }
+
+    }
+    for (i = 0; i < rows.length; i++) {
+        rows[i].onclick = function () {
+            return function () {
+                featureOverlay.getSource().clear();
+
+                $(function () {
+                    $("#attQryTable td").each(function () {
+                        $(this).parent("tr").css("background-color", "white");
+                    });
+                });
+                var cell = this.cells[col_no - 1];
+                var id = cell.innerHTML;
+                $(document).ready(function () {
+                    $("#attQryTable td:nth-child(" + col_no + ")").each(function () {
+                        if ($(this).text() == id) {
+                            $(this).parent("tr").css("background-color", "#d1d8e2");
+                        }
+                    });
+                });
+
+                var features = geojson.getSource().getFeatures();
+
+                for (i = 0; i < features.length; i++) {
+                    if (features[i].getId() == id) {
+                        featureOverlay.getSource().addFeature(features[i]);
+
+                        featureOverlay.getSource().on('addfeature', function () {
+                            map.getView().fit(
+                                featureOverlay.getSource().getExtent(),
+                                { duration: 1500, size: map.getSize(), maxZoom: 24 }
+                            );
+                        });
+
+                    }
+                }
+            };
+        }(rows[i]);
+    }
+}
+
+// end : attribute query
+
+//Start Geolocate
+var geolocateInteraction = new ol.interaction.Geolocation();
+
+geolocateInteraction.on('change:position', function() {
+    var coordinates = geolocateInteraction.getPosition();
+    map.getView().setCenter(coordinates);
+});
+
+var geolocateButton = document.createElement('button');
+geolocateButton.innerHTML = '<img src="resources/images/Geo.png" alt="" style="width:18px;height:18px;"></img>';
+geolocateButton.className = 'myButton';
+geolocateButton.id = 'geolocateButton';
+
+var geolocateElement = document.createElement('div');
+geolocateElement.className = 'geolocateButtonDiv';
+geolocateElement.appendChild(geolocateButton);
+
+var geolocateControl = new ol.control.Control({
+    element: glElement
+});
+
+var geolocateFlag = false;
+geolocateButton.addEventListener("click", () => {
+    geolocateButton.classList.toggle('clicked');
+    geolocateFlag = !geolocateFlag;
+    if (geolocateFlag) {
+        map.addInteraction(geolocateInteraction);
+    } else {
+        map.removeInteraction(geolocateInteraction);
+    }
+});
+
+map.addControl(geolocateControl);
+
+//End Geolocate
+
+
+// end : attribute query
